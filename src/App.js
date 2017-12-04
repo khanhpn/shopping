244a48aaae90f7fe39f9152a8e14c158ea38f7c1
khanhpn <khanhpham@bravesoft.vn>
2017-12-04 08:13:40 +0700

    license

--- src/App.js
+++ src/App.js
@@ -6,15 +6,50 @@ import Product from './components/Product';
 
 class App extends Component {
   render() {
+    var products = [
+      {
+        id: 1,
+        name: "iphone",
+        status: true,
+        price: 1000000,
+        description: "this is special iphone"
+      },
+      {
+        id: 2,
+        name: "samsung",
+        status: true,
+        price: 1000000,
+        description: "this is special iphone"
+      },
+      {
+        id: 3,
+        name: "lg",
+        status: true,
+        price: 1000000,
+        description: "this is special iphone"
+      },
+      {
+        id: 4,
+        name: "asus",
+        status: true,
+        price: 1000000,
+        description: "this is special iphone"
+      }
+    ];
+
+    let elements = products.map((product, index) => {
+      return(
+        <Product id={product.id} name={product.name} price={product.price} status={product.status}>
+        {product.description}
+        </Product>
+      );
+    });
     return (
       <div className="container-fluid">
         <Header />
         <div className="row">
           <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-            <Product />  
-          </div>
-          <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-          <Product />
+            {elements}
           </div>
         </div>
         
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 
 export default class Product extends Component {
-  showInfoProduct(product) {
-    if(product.display) {
+  showButtonCart(status) {
+    if(status == "true") {
       return(
         <div className="card-block">
           <a href="#" className="btn btn-success">Mua hàng</a>
@@ -10,56 +10,22 @@ export default class Product extends Component {
       );
     }
   }
+  
   render() {
-    var a = 5;
-    var product = {
-      id: 1,
-      name: "galaxy",
-      price: 1000000,
-      display: true
-    };
-
-    var users = [
-      {
-        id: 1,
-        name: "khanh",
-        age: 27
-      },
-      {
-        id: 2,
-        name: "ngoc",
-        age: 19
-      },
-      {
-        id: 3,
-        name: "pham",
-        age: 18
-      }
-    ];
-    var elements = users.map((user, index) => {
-      return (
-        <div key={user.id}>
-          <p>name: {user.name}</p>
-          <hr />
-          <p>age: {user.age}</p>
-        </div>
-      );
-    });
     return (
       <div className="card">
         <img className="card-img-top" src="https://support.apple.com/content/dam/edam/applecare/images/en_US/iphone/content-link-iphone-transfer-content-ios11_2x.png" alt="Card image cap" />
         <div className="card-block">
-          <h4 className="card-title">{product.name}</h4>
+          <h4 className="card-title">{this.props.name}</h4>
           <p className="card-text">
           With iOS 11, moving information to a new iPhone just got easier.
+          {this.props.children}
           </p>
         </div>
         <ul className="list-group list-group-flush">
-          <li className="list-group-item">{product.price} vnd</li>
+          <li className="list-group-item">{this.props.price} vnd</li>
         </ul>
-        {this.showInfoProduct(product)}
-        <br />
-        {elements}
+        {this.showButtonCart(this.props.status)}
       </div>
     )
   }
