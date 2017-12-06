790724ad2faabfb6a401fff0d17f9c38a53d6172
khanhpn <khanhpham@bravesoft.vn>
2017-12-06 21:20:19 +0900

    keep datetime

--- src/App.js
+++ src/App.js
@@ -4,41 +4,48 @@ import './App.css';
 import Header from './components/Header';
 import Product from './components/Product';
 import Form from './components/Form';
+import Search from './components/search';
+import Clock from './components/clock';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: [
+        {
+          id: 1,
+          name: "iphone",
+          status: true,
+          price: 1000000,
+          description: "this is special iphone"
+        },
+        {
+          id: 2,
+          name: "samsung",
+          status: true,
+          price: 1000000,
+          description: "this is special iphone"
+        },
+        {
+          id: 3,
+          name: "lg",
+          status: true,
+          price: 1000000,
+          description: "this is special iphone"
+        },
+        {
+          id: 4,
+          name: "asus",
+          status: true,
+          price: 1000000,
+          description: "this is special iphone"
+        }
+      ],
+      isActive: true
+    };
+  }
   render() {
-    var products = [
-      {
-        id: 1,
-        name: "iphone",
-        status: true,
-        price: 1000000,
-        description: "this is special iphone"
-      },
-      {
-        id: 2,
-        name: "samsung",
-        status: true,
-        price: 1000000,
-        description: "this is special iphone"
-      },
-      {
-        id: 3,
-        name: "lg",
-        status: true,
-        price: 1000000,
-        description: "this is special iphone"
-      },
-      {
-        id: 4,
-        name: "asus",
-        status: true,
-        price: 1000000,
-        description: "this is special iphone"
-      }
-    ];
-
-    let elements = products.map((product, index) => {
+    let elements = this.state.products.map((product, index) => {
       return(
         <Product id={product.id} name={product.name} price={product.price} status={product.status} key={index}>
         {product.description}
@@ -47,6 +54,8 @@ class App extends Component {
     });
     return (
       <div className="container-fluid">
+        <Clock />
+        <Search />
         <Header />
         <div className="row">
           <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
