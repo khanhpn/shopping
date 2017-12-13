d016bdbb83b47a9994b90ecd89fe4400400e4e8c
khanhpn <khanhpham@bravesoft.vn>
2017-12-13 21:11:52 +0900

    minatoku 427

--- src/components/colorpicker.js
+++ src/components/colorpicker.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react'
 
 export default class ColorPicker extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      colors: ["green", "blue", "yellow", "purple"]
+    };
+  }
+
+  showColor(color) {
+    return {
+      backgroundColor: color,
+      padding: "40px",
+      margin: "20px",
+      marginBottom: "-40px"
+    };
+  }
+
   render() {
+    var elementColors = this.state.colors.map((color, index) => {
+      return <span key={index} style={this.showColor(color)}></span>
+    });
     return (
       <div className="col-sm-6">
         <div className="panel panel-primary">
           <div className="panel-heading">Color picker</div>
         </div>
         <div className="panel-body">
-          <span></span>
+          {elementColors}
           <hr/>
         </div>
       </div>
