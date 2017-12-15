72c8160851136e4fef8ed8962598b2b6cc8c6df1
khanhpn <khanhpham@bravesoft.vn>
2017-12-15 15:39:40 +0900

    form reactjs

--- src/App.js
+++ src/App.js
@@ -1,68 +1,114 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header';
-import Product from './components/Product';
-import Form from './components/Form';
-import Search from './components/search';
-import Clock from './components/clock';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      products: [
-        {
-          id: 1,
-          name: "iphone",
-          status: true,
-          price: 1000000,
-          description: "this is special iphone"
-        },
-        {
-          id: 2,
-          name: "samsung",
-          status: true,
-          price: 1000000,
-          description: "this is special iphone"
-        },
-        {
-          id: 3,
-          name: "lg",
-          status: true,
-          price: 1000000,
-          description: "this is special iphone"
-        },
-        {
-          id: 4,
-          name: "asus",
-          status: true,
-          price: 1000000,
-          description: "this is special iphone"
-        }
-      ],
-      isActive: true
+      txtName: "sample username",
+      txtPassword: "",
+      txtContent: "",
+      sltGender: 1,
+      rdLanguage: 'ja',
+      chkbStatus: true
     };
+    this.onHandleChange = this.onHandleChange.bind(this);
+    this.onHandleSubmit = this.onHandleSubmit.bind(this);
   }
-  render() {
-    let elements = this.state.products.map((product, index) => {
-      return(
-        <Product id={product.id} name={product.name} price={product.price} status={product.status} key={index}>
-        {product.description}
-        </Product>
-      );
+
+  onHandleChange(event) {
+    var target = event.target;
+    var name = target.name;
+    var value = target.type === "checkbox" ? target.checked : target.value;
+
+    this.setState({
+      [name]: value
     });
+  }
+
+  onHandleSubmit(event) {
+    event.preventDefault();
+    console.log(this.state);
+  }
+
+  render() {
     return (
       <div className="container-fluid">
-        <Clock />
-        <Search />
-        <Header />
         <div className="row">
-          <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-            {elements}
+          <div className="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+            <div className="panel panel-primary">
+                <div className="panel-heading">
+                  <h3 className="panel-title">Form</h3>
+                </div>
+                <div className="panel-body">
+                  <form onSubmit={this.onHandleSubmit}>
+                      <div className="form-group">
+                        <label>username:</label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          name="txtName"
+                          value={this.state.txtName}
+                          onChange={this.onHandleChange}/>
+                      </div>
+                      <div className="form-group">
+                        <label>password:</label>
+                        <input
+                          type="password"
+                          className="form-control"
+                          name="txtPassword"
+                          onChange={this.onHandleChange}/>
+                      </div>
+                      <div className="form-group">
+                        <label>Content:</label>
+                        <textarea className="form-control" rows="3" name="txtContent" onChange={this.onHandleChange}></textarea>
+                      </div>
+                      <label>Gender</label>
+                      <select className="form-control" name="sltGender" value={this.state.sltGender} onChange={this.onHandleChange}>
+                        <option value={0}>Female</option>
+                        <option value={1}>Male</option>
+                      </select>
+                      <label>Language</label>
+                      <div className="radio">
+                        <label>
+                          <input
+                            type="radio"
+                            name="rdLanguage"
+                            id="input${1/(\w+)/\u\1/g}"
+                            value="en"
+                            onChange={this.onHandleChange}
+                            checked={this.state.rdLanguage === 'en'} />
+                          English
+                        </label>
+                        <br />
+                        <label>
+                          <input
+                            type="radio"
+                            name="rdLanguage"
+                            id="input${1/(\w+)/\u\1/g}"
+                            value="ja"
+                            onChange={this.onHandleChange}
+                            checked={this.state.rdLanguage === 'ja'} />
+                          Japanese
+                        </label>
+                      </div>
+                      <div className="checkbox">
+                        <label>
+                          <input type="checkbox" value={true} name="chkbStatus" onChange={this.onHandleChange} checked={this.state.chkbStatus === true} />
+                          Status
+                        </label>
+                      </div>
+                      <div className="form-group">
+                        <button type="submit" className="btn btn-primary">Submit</button>
+                        &nbsp;
+                        <button type="reset" className="btn btn-success">Reset</button>
+                      </div>
+                  </form>
+                </div>
+            </div>
           </div>
         </div>
-        <Form />
       </div>
     );
   }
