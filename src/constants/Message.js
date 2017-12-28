2597f791365f09c0248aa4efc2225d736d96fcd7
khanhpn <khanhpham@bravesoft.vn>
2017-12-28 18:51:05 +0900

    weather icon wicon

---
+++ src/constants/Message.js
@@ -0,0 +1,5 @@
+export const MSG_ADD_TO_CAR_SUCCESS = "Buy product successfully.";
+export const MSG_UPDATE_CART_SUCCESS = "Update cart successfully."
+export const MSG_DELETE_PRODUCT_IN_CART = "Remove item from cart."
+export const MSG_CART_EMPTY = "Empty product in cart."
+export const MSG_WELCOME = "Welcome to shopping online."
